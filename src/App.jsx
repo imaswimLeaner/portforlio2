20e23720a5e3ed04511d302006eefd74f49e52b5
Jonathan Convento <convento_nathan@yahoo.com>
2022-03-06 18:49:28 +0100

    updating package

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Toggle from "./components/toggle/Toggle";
 import { ThemeContext } from "./context";
-import "./sass/main.scss";
+
 
 
 const App = () => {
